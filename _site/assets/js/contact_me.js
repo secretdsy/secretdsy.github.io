8ff1a89ebc6714b763c5f07bf811789ad02ebbe0
Doyoung Kim <secretdsy@gmail.com>
2024-05-13 18:47:50 +0900

    .

--- _site/assets/js/contact_me.js
+++ _site/assets/js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "dykim@msjcorp.co.kr";
+	  var url = "https://formspree.io/" + "admin@msjcorp.co.kr";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
